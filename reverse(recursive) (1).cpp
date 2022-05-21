0fb21fc81698fae403bf3d28f5d0cea8124d9bdc
Jatin Parashar <78733719+ParasharJatin333@users.noreply.github.com>
2022-05-21 22:01:44 +0530

    Linked List

---
+++ reverse(recursive) (1).cpp
@@ -0,0 +1,33 @@
+//Write a Program to reverse the Linked List. (recursive)
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ft first
+#define sc second
+#define pb push_back
+#define ins insert
+#define all(v) v.begin(), v.end()
+#define Sort(v) sort(all(v))
+#define s(a, n) sort(a, a + n)
+#define f1(i,a,b) for(ll i=a;i<b;i++)
+#define f2(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define f3(i,a,b) for(ll i=a;i>=b;i--)
+#define f4(i,a,b,c) for(ll i=a;i>=b;i-=c)
+#define jatin(t) int t; cin >> t; while(t--)
+const int N = 1e5+2, MOD = 1e9+7;
+//int array[N];
+//cout << (condition ? "YES\n" : "NO\n");
+    Node* reverseList(Node *head)
+    {
+        // code here
+        // return head of reversed list
+        if(!head || !head->next){
+            return head;
+        }
+        Node* rest= reverseList(head->next);
+        head->next->next=head;
+        
+        head->next=NULL;
+        
+        return rest;
+    }
